b55f6ebc39c0484951d2ca2af4846be1c3a0e8fa
lokesh <lokesh.koundal@quytech.com>
2024-12-30 12:38:05 +0530

    adding a music player UI

--- app/src/main/java/com/lokesh/composetutorial/audioPlayerApp/ui/HomeScreen.kt
+++ app/src/main/java/com/lokesh/composetutorial/audioPlayerApp/ui/HomeScreen.kt
@@ -5,22 +5,33 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -40,45 +51,86 @@ fun HomeScreen(){
         .padding(18.dp)
         ) {
         TopBar()
+        Spacer(modifier = Modifier.height(10.dp))
+        TitleNSearchRow()
     }
 }
 
 
 @Composable
 fun TopBar(){
-    Row(modifier = Modifier.fillMaxWidth().padding(0.dp,0.dp,0.dp,10.dp),
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .padding(0.dp, 0.dp, 0.dp, 10.dp),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceBetween) {
 
         Row {
-            Image(modifier = Modifier
-                .size(65.dp)
-                .clip(CircleShape)
-                .border(1.dp, Color.White, CircleShape),
+            Image(
+                modifier = Modifier
+                    .size(65.dp)
+                    .clip(CircleShape)
+                    .border(1.dp, Color.White, CircleShape),
                 contentScale = ContentScale.Crop,
                 painter = painterResource(id = R.drawable.user_dp),
                 contentDescription = "dp",
                 alignment = Alignment.Center
-              )
+            )
 
             Spacer(modifier = Modifier.padding(6.dp))
 
-            Column(horizontalAlignment = Alignment.Start,
-                verticalArrangement = Arrangement.Center) {
+            Column(
+                horizontalAlignment = Alignment.Start,
+                verticalArrangement = Arrangement.Center
+            ) {
 
-                Text(text = "Lokesh Android", fontSize = 22.sp,
-                    fontFamily = FontFamily(Font(R.font.nunito_bold) ), color = Color.White)
+                Text(
+                    text = "Lokesh Android", fontSize = 22.sp,
+                    fontFamily = FontFamily(Font(R.font.nunito_bold)), color = Color.LightGray
+                )
 
-                Text(text = "Gold Member", fontSize = 18.sp, color = Color.White,
-                    fontFamily = FontFamily(Font(R.font.nunito_bold)))
+                Text(
+                    text = "Gold Member", fontSize = 18.sp, color = Color.LightGray,
+                    fontFamily = FontFamily(Font(R.font.nunito_bold))
+                )
             }
 
-            Button(onClick = { }, modifier = Modifier.size(32.dp).clip(CircleShape).background(Color.Transparent)) {
+        }
+            Button(onClick = { }, modifier = Modifier
+                .size(36.dp)
+                .background(Color.Transparent),
+                shape = RoundedCornerShape(6.dp),
+                colors = ButtonDefaults.buttonColors(Color(0xFF181557)),
+                contentPadding = PaddingValues(0.dp)
+            ) {
                 Image(imageVector = ImageVector.vectorResource(id = R.drawable.ic_bell), contentDescription = "bell",
                     modifier = Modifier.fillMaxSize())
             }
 
-        }
+    }
+}
+
+@Composable
+fun TitleNSearchRow(){
+//    var isActive by remember { mutableStateOf(false) }
+
+    Row (horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically){
+
+        Text(text = "Listen the Latest Musics",
+            fontFamily = FontFamily(Font(R.font.nunito_bold)),
+            fontSize = 20.sp,
+            color = Color.White)
+
+
+//        SearchBar(
+//            inputField = { TextField(value = , onValueChange = ) },
+//            expanded = false,
+//            onExpandedChange = {},
+//            active = isActive,
+//        ) {
+//
+//        }
 
     }
 }
