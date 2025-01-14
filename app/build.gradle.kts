52b6b331bcb0614e7a0a17a7ca7c1539c0184f15
Lokesh <lokesh.koundal@quytech.com>
2025-01-14 18:48:39 +0530

    compose navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
     id ("kotlin-kapt")
     id("com.google.dagger.hilt.android")
     id("androidx.navigation.safeargs.kotlin")
+    kotlin("plugin.serialization") version "2.0.21"
+
 
 }
 
@@ -80,6 +82,8 @@ dependencies {
 
     //navigation
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.kotlinx.serialization.json)
+
 
 
     implementation(libs.androidx.core.ktx)
--- app/src/main/java/com/lokesh/composetutorial/instagram/ui/InstagramActivity.kt
+++ app/src/main/java/com/lokesh/composetutorial/instagram/ui/InstagramActivity.kt
@@ -10,25 +10,64 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.lokesh.composetutorial.audioPlayerApp.ui.HomeScreen
 import com.lokesh.composetutorial.instagram.ui.composeElements.BottomNavigationBar
 import com.lokesh.composetutorial.instagram.ui.ui.theme.ComposeTutorialTheme
+import kotlinx.serialization.Serializable
+
+@Serializable
+object Home
+
+@Serializable
+object Search
+
+@Serializable
+object Add
+
+@Serializable
+object Reels
+
+@Serializable
+object Profile
 
 class InstagramActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeTutorialTheme {
+
+                val navController = rememberNavController()
+
                 Scaffold(modifier = Modifier.fillMaxSize(),
                     bottomBar = {
-                        BottomNavigationBar()
+                        BottomNavigationBar(navController = navController,
+                            currentRoute = "x",
+                            onItemSelected = {})
                     }) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+
+                    InstagramNavHost(
+                        navController = navController,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
+    }
+}
+
+@Composable
+fun InstagramNavHost(navController: NavHostController, modifier: Modifier) {
+    NavHost(navController = navController,
+        startDestination = Home){
+        composable<Home>{ HomeScreen() }
+        composable<Search>{}
+        composable<Add>{}
+        composable<Reels>{}
+        composable<Profile>{}
     }
 }
 
